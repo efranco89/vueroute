84ee97fc9c1121df4556841890a7e8eb8b2e5555
Enrique Franco <luisfrancoz1@gmail.com>
2020-01-02 16:45:41 -0500

    Ref Routing with params and events 2

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -2,7 +2,7 @@
   <div class="users">
     <h1>Informacion del Usuario</h1>
     <router-link :to="{ name:'users', params: {id: user } }"
-      v-for="(user) in usersArray" :key="user">
+      v-for="(user, index) in usersArray" :key="index">
       <button type="button" name="button">{{ user }}</button>
     </router-link>
     <User/>
