e0848b7ff319fab7e863c5814fd6e7e10419d647
Enrique Franco <luisfrancoz1@gmail.com>
2020-01-02 19:20:36 -0500

    Ref adding more products to the view

--- src/views/products/Index.vue
+++ src/views/products/Index.vue
@@ -10,11 +10,46 @@
             <img src="https://placehold.it/200x100/000" alt="">
           </div>
           <div class="col-md-12">
-            <h4>Product Title</h4>
+            <h4>Product Title 1</h4>
             <p>Product Description</p>
             <a href="#" class="btn btn-success btn-sm">Agregar</a>
           </div>
         </div>
+
+        <div class="col-md-3">
+          <div class="col-md-12">
+            <img src="https://placehold.it/200x100/000" alt="">
+          </div>
+          <div class="col-md-12">
+            <h4>Product Title 2</h4>
+            <p>Product Description</p>
+            <a href="#" class="btn btn-success btn-sm">Agregar</a>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="col-md-12">
+            <img src="https://placehold.it/200x100/000" alt="">
+          </div>
+          <div class="col-md-12">
+            <h4>Product Title 3</h4>
+            <p>Product Description</p>
+            <a href="#" class="btn btn-success btn-sm">Agregar</a>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="col-md-12">
+            <img src="https://placehold.it/200x100/000" alt="">
+          </div>
+          <div class="col-md-12">
+            <h4>Product Title 4</h4>
+            <p>Product Description</p>
+            <a href="#" class="btn btn-success btn-sm">Agregar</a>
+          </div>
+        </div>
+
+        
       </div>
     </div>
   </div>
